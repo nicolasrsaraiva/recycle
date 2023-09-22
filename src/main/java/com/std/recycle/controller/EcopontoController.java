e6866cdcb89de461d18b30bc3b455cfbc19dcb7f
Nícolas Saraiva <132316573+nicolasrsaraiva@users.noreply.github.com>
2023-09-21 22:38:56 -0300

    feat: adicionado propriedades da aplicacao

--- src/main/java/com/std/recycle/controller/EcopontoController.java
+++ src/main/java/com/std/recycle/controller/EcopontoController.java
@@ -1,18 +1,22 @@
 package com.std.recycle.controller;
 
 import com.std.recycle.controller.dtos.EcopontoDTO;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 
 @RestController
+@RequestMapping("/ecopontos")
 public class EcopontoController {
 
     @PostMapping
     public void cadastrar(){
     }
 
+    @GetMapping
     public ArrayList<EcopontoDTO> listarEcopontos(){
         ArrayList<EcopontoDTO> ecopontoDTOS = new ArrayList<>();
         return ecopontoDTOS;
